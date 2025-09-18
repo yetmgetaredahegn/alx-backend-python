11984dec1690d8c199f453e67857c7aba6ac921f
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-09-18 16:13:27 +0300

    memoization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ from unittest import result
 from unittest.mock import Mock, patch
 from parameterized import parameterized
 
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -40,3 +40,32 @@ class TestGetJson(unittest.TestCase):
 
         self.assertEqual(result,test_payload)
         mock_get.assert_called_once_with(test_url)
+
+
+#!/usr/bin/env python3
+import unittest
+from unittest.mock import patch
+from utils import memoize
+
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        # Patch a_method so we can count calls
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+
+            # First access calls a_method
+            self.assertEqual(obj.a_property, 42)
+            mock_method.assert_called_once()
+
+            # Second access should not call a_method again (cached)
+            self.assertEqual(obj.a_property, 42)
+            mock_method.assert_called_once()  # still only one call
