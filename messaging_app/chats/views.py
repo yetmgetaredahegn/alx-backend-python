65b774882388879eaa51aab21efe3586d2b3c750
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-09-20 22:21:14 +0300

    viewsets

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    @action(detail=True, methods=["post"])
+    def add_message(self, request, pk=None):
+        """
+        Custom action to send a message inside a conversation.
+        Endpoint: POST /api/conversations/{id}/add_message/
+        """
+        conversation = get_object_or_404(Conversation, pk=pk)
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(sender=request.user, conversation=conversation)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        """
+        Ensure sender is always the logged-in user when creating a message.
+        """
+        serializer.save(sender=self.request.user)
