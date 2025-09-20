2f28720f4adeb28e821ea329b59d33333f286aae
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-09-20 22:29:21 +0300

    viewsets

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -11,18 +11,28 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
 
-    def perform_create(self, serializer):
+    def list(self, request, *args, **kwargs):
         """
-        When creating a new conversation, add the requesting user as a participant.
+        Explicitly list all conversations.
         """
+        conversations = Conversation.objects.all()
+        serializer = self.get_serializer(conversations, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        """
+        Explicitly create a new conversation and add the requesting user as a participant.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         conversation = serializer.save()
-        conversation.participants.add(self.request.user)
+        conversation.participants.add(request.user)
+        return Response(self.get_serializer(conversation).data, status=status.HTTP_201_CREATED)
 
     @action(detail=True, methods=["post"])
     def send_message(self, request, pk=None):
         """
         Custom action to send a message in an existing conversation.
-        Endpoint: POST /api/conversations/{id}/send_message/
         """
         conversation = get_object_or_404(Conversation, pk=pk)
         serializer = MessageSerializer(data=request.data)
@@ -36,8 +46,19 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
-    def perform_create(self, serializer):
+    def list(self, request, *args, **kwargs):
         """
-        Always set sender to logged-in user when creating a message.
+        Explicitly list all messages.
         """
-        serializer.save(sender=self.request.user)
+        messages = Message.objects.all()
+        serializer = self.get_serializer(messages, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        """
+        Explicitly create a message. Sender is always the logged-in user.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(sender=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
