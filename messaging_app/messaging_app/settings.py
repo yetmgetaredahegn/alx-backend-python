7a339f8eb85e6b96d29e96b7414d5b3a0acb45f4
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-09-22 18:21:02 +0300

    task 2

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    "DEFAULT_PAGINATION_CLASS": "chats.pagination.DefaultPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",  
     "PAGE_SIZE": 20,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
